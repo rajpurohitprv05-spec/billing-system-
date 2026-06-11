7b9f36981ee7da2a4bb07b84540a35a66aa1be6d
pranav <rajpurohitprv05@gmail.com>
2026-06-11 18:01:28 +0530

    choice and quantity

--- main.c
+++ main.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
 
-int main() {
-    char Apple;
-    char Banana;
-    char Grapes;
-
-    printf("Enter the quantity of Apple: ");
-    scanf("%c", &Apple);
-    printf("Enter the quantity of Banana: ");
-    scanf(" %c", &Banana);
-    printf("Enter the quantity of Grapes: ");
-    scanf(" %c", &Grapes);
+int main() 
+{
+    int choice, quantity;
     
+    printf("fruit shop\n");
+    printf("1. apple\n");
+    printf("2. Banana\n");
+    printf("3. Grapes\n");
+
+    printf("Please select a fruit (1-3): ");
+    scanf("%d", &choice);
+
+    printf("how many kg do you want to buy? ");
+    scanf("%d", &quantity);
+
+    switch (choice) {
+        case 1:
+            printf("You selected apple : %d kg\n", quantity);
+            break;
+        case 2:
+            printf("You selected Banana : %d kg\n", quantity);
+            break;
+        case 3:
+            printf("You selected Grapes : %d kg\n", quantity);
+            break;
+        default:
+            printf("Invalid selection\n");
+    }
+
     return 0;
 }
