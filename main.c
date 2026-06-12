d02a8b3235ad7f180417d66aff4028fd5470ccd3
pranav <rajpurohitprv05@gmail.com>
2026-06-12 14:39:47 +0530

    system

--- main.c
+++ main.c
@@ -6,38 +6,74 @@ int main()
     char again;
     float quantity;
     
+    int apple = 1 , banana = 1, grapes = 1;
     do
     {
          printf("fruit shop\n");
+
+         if(apple)
          printf("1. apple\n");
+
+         if(banana)
          printf("2. Banana\n");
+
+         if(grapes)
          printf("3. Grapes\n");
 
         printf("Please select a fruit (1-3): ");
         scanf("%d", &choice);
 
-        printf("how many kg do you want to buy? ");
-        scanf("%f", &quantity);
-
-        switch (choice) {
-            case 1:
-            printf("You selected apple : %.2f kg\n", quantity);
+        switch (choice)
+        {
+        case 1:
+            if(apple)
+            {
+                printf("how many kg do you want to buy? ");
+                scanf("%f", &quantity);
+                printf("You selected apple : %.2f kg\n", quantity);
+                 apple = 0;
+            }
+            else
+                printf("apple are alerady purchased\n");
             break;
-            case 2:
-            printf("You selected Banana : %.2f kg\n", quantity);
+        case 2:
+            if(banana)            
+            {
+                printf("how many kg do you want to buy? ");
+                scanf("%f", &quantity);
+                printf("You selected Banana : %.2f kg\n", quantity);
+                 banana = 0;
+            }
+            else
+                printf("Banana are alerady purchased\n");
             break;
-            case 3:
-            printf("You selected Grapes : %.2f kg\n", quantity);
+        case 3: 
+            if(grapes)            
+            {
+                printf("how many kg do you want to buy? ");
+                scanf("%f", &quantity);
+                printf("You selected Grapes : %.2f kg\n", quantity);
+                 grapes = 0;
+            }
+            else
+                printf("Grapes are alerady purchased\n");
             break;
-            default:
-            printf("Invalid selection\n");
+        default:
+            printf("Invalid selection\n");      
             break;
         }
+
+        if(!apple && !banana && !grapes)
+        {
+            printf("All fruits are purchased\n");
+            break;
+        }
+
         printf("Do you want to keep shopping? (y for yes, n for no): ");
         scanf(" %c", &again);
     } while (again == 'y' || again == 'Y');
 
     printf("Thank you for shopping with us!\n");
-    
+
     return 0;
 }
