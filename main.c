e66fe615955cf902ada2460ac534aa17d7548eb8
pranav <rajpurohitprv05@gmail.com>
2026-06-12 14:31:23 +0530

    loop

--- main.c
+++ main.c
@@ -2,62 +2,42 @@
 
 int main() 
 {
-    int choice, quantity;
+    int choice;
+    char again;
+    float quantity;
     
-    printf("fruit shop\n");
-    printf("1. apple\n");
-    printf("2. Banana\n");
-    printf("3. Grapes\n");
+    do
+    {
+         printf("fruit shop\n");
+         printf("1. apple\n");
+         printf("2. Banana\n");
+         printf("3. Grapes\n");
 
-    printf("Please select a fruit (1-3): ");
-    scanf("%d", &choice);
+        printf("Please select a fruit (1-3): ");
+        scanf("%d", &choice);
 
-    printf("how many kg do you want to buy? ");
-    scanf("%d", &quantity);
+        printf("how many kg do you want to buy? ");
+        scanf("%f", &quantity);
 
-    switch (choice) {
-        case 1:
-            printf("You selected apple : %d kg\n", quantity);
+        switch (choice) {
+            case 1:
+            printf("You selected apple : %.2f kg\n", quantity);
             break;
-        case 2:
-            printf("You selected Banana : %d kg\n", quantity);
+            case 2:
+            printf("You selected Banana : %.2f kg\n", quantity);
             break;
-        case 3:
-            printf("You selected Grapes : %d kg\n", quantity);
+            case 3:
+            printf("You selected Grapes : %.2f kg\n", quantity);
             break;
-        default:
+            default:
             printf("Invalid selection\n");
-
-
-    }
-
-    printf("Do you want to keep shopping? (y for yes, n for no): ");
-    scanf(" %c", &choice);
-
-    if (choice == 'y' || choice == 'Y')
-    {
-        printf("nAvailable fruits:\n");
-
-        if(choice != 1)
-        {
-            printf("1. apple\n");
+            break;
         }
-        else if(choice != 2)
-        {
-            printf("2. Banana\n");
-        }
-        else if(choice != 3)
-        {
-            printf("3. Grapes\n");
-        }   
-        /* code */
-    }
+        printf("Do you want to keep shopping? (y for yes, n for no): ");
+        scanf(" %c", &again);
+    } while (again == 'y' || again == 'Y');
 
-    else
-    {
-        printf("Thank you for shopping with us!\n");
-    }
+    printf("Thank you for shopping with us!\n");
     
-
     return 0;
 }
